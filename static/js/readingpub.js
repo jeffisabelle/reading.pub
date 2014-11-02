7dd5e392bb505da926022daef8e04859053c24cc
jeffisabelle <muhitosan@gmail.com>
2014-11-03 00:54:35 +0200

    highlighting bugs fixed

--- static/js/readingpub.js
+++ static/js/readingpub.js
@@ -28,9 +28,109 @@ $(document).ready(function() {
     }, function() {
         $(this).find('.thumbnail-hover').fadeOut(100);
     });
+
+    $('.page-wrap').mouseup(function(e) {
+        if(getSelectedText() != "") {
+            // console.log(getSelectionCoords());
+            var sel = window.getSelection();
+            var range = sel.getRangeAt(0).cloneRange();
+            var boundingRect = range.getBoundingClientRect();
+            var yOffset = window.pageYOffset;
+
+            // console.log(range.getClientRects());
+            var rect = boundingRect;
+            var left = rect.left;
+            var right = rect.right;
+            var top = rect.top + yOffset;
+            var bottom = rect.bottom;
+            var width = rect.width;
+            var height = rect.height;
+            $(".highlight-wrapper").css("width", width);
+            $(".highlight-wrapper").css("height", height);
+            $(".highlight-wrapper").css("left", left);
+            $(".highlight-wrapper").css("top", top);
+            $(".highlight-wrapper").show();
+        } else {
+            $(".highlight-wrapper").hide();
+        }
+    });
+
+    $('.highlight-wrapper').click(function(e) {
+        $(".highlight-wrapper").hide();
+    });
+
+    $('.highlighted').click(function() {
+        var html = $(this).html()
+        $(this).after(html);
+        $(this).remove();
+        syncData();
+    });
+
 });
 
+function getSelectedText() {
+    if (window.getSelection) {
+        return window.getSelection().toString();
+    } else if (document.selection) {
+        return document.selection.createRange().text;
+    }
+    return '';
+}
 
+function highlightSelected() {
+    var sel = window.getSelection();
+
+    var div = document.createElement("div");
+    div.className = "highlighted";
+
+    if (sel.rangeCount) {
+        var range = sel.getRangeAt(0).cloneRange();
+        try {
+            range.surroundContents(div);
+        } catch(err) {
+            alert("multiple paragraph highlighting not supported. (yet)");
+        }
+
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    syncData();
+
+    $('.highlighted').click(function() {
+        var html = $(this).html()
+        $(this).after(html);
+        $(this).remove();
+        syncData();
+    });
+}
+
+function syncData() {
+    // send content to server replace post-content
+    var html = $(".content-data").html();
+    var postid = $(".content-data").data("id");
+    var currenturl = $(location).attr("href");
+
+    var data = {
+        content: html,
+        postid: postid
+    }
+
+    $.ajax({
+        type: "POST",
+        url : "/post/sync",
+        data: JSON.stringify(data, null, '\t'),
+        contentType: 'application/json;charset=UTF-8',
+        success: function(result) {
+            var res = JSON.parse(result);
+            if(res.status == "success") {
+                // window.location.href = currenturl;
+            }
+        },
+        error: function(result) {
+            console.log(result);
+        }
+    });
+}
 
 // used on base
 function login() {
